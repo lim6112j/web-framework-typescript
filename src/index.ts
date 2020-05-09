ed3d69bab6e48c6547b1a4bf869f6e6d7e4a92b3
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2020-05-09 15:12:13 +0900

    event system

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { User } from './models/User';
+
+const user = new User({name: 'lim', age:48});
+user.on('click', () => {
+  console.log('click event 1 triggered')
+});
+user.on('click', () => {
+  console.log('click event 2 triggered')
+})
+setTimeout(() => {
+  user.trigger('click')
+}, 1000);
+console.log(user)
